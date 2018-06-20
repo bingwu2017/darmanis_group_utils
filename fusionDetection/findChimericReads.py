6471e2f8e956136d8c9d3e92ac315cec8bd0aee9
Lincoln Harris <lincoln.harris@harris-mbp.local>
2018-06-20 16:24:39 -0700

    updated findChimeric

--- fusionDetection/findChimericReads.py
+++ fusionDetection/findChimericReads.py
@@ -2,7 +2,7 @@
 #/////////////////////////////////////////////////////////////////////
 #/////////////////////////////////////////////////////////////////////
 #/////////////////////////////////////////////////////////////////////
-# script: findChimericReads.py
+# script: findChimericReads_test.py
 # author: Lincoln Harris
 # date: 6/18/18
 #
@@ -10,6 +10,9 @@
 # the names of cells that contain chimeric reads ie. cells such that for
 # a given read pair, one read matches geneA, and the other matches geneB.
 # Right now configured for Alk/Eml4 searches. 
+#
+# Want to run this on a directory I've seen chimeric reads for in the
+# past. 
 #/////////////////////////////////////////////////////////////////////
 #/////////////////////////////////////////////////////////////////////
 import os
@@ -25,9 +28,22 @@ import sys
 #
 #/////////////////////////////////////////////////////////////////////
 def findReadPairs(l1, l2):
-	l1_set = set(l1)
-	l2_set = set(l2)
+
+	l1_new = []
+	for line1 in l1:
+		newLine1 = line1.split(" ")[0]
+		l1_new.append(newLine1)
+
+	l2_new = []
+	for line2 in l2:
+		newLine2 = line2.split(" ")[0]
+		l2_new.append(newLine2)
+
+	l1_set = set(l1_new)
+	l2_set = set(l2_new)
+
 	shared = l1_set.intersection(l2_set)
+
 	return shared
 
 #/////////////////////////////////////////////////////////////////////
@@ -62,7 +78,8 @@ def findHitSeqs(fileName):
 #
 #/////////////////////////////////////////////////////////////////////
 
-PATH = '/home/ubuntu/expansionVol/03-fusionMessingAround/15704_clean'
+PATH = '/home/ubuntu/expansionVol/03-fusionMessingAround/180307_A00111_0109_BH3KCJDSXX_copy'
+#PATH = '/home/ubuntu/expansionVol/03-fusionMessingAround/17504_clean'
 
 for dir in os.listdir(PATH):
 	dirPath = PATH + '/' + dir
@@ -72,40 +89,32 @@ for dir in os.listdir(PATH):
 	f3 = dirPath + '/' + dir + '_' + 'eml4_R1_blastOut'
 	f4 = dirPath + '/' + dir + '_' + 'eml4_R2_blastOut'
 
-	f1_hit = False
-	f2_hit = False
-	f3_hit = False
-	f4_hit = False
+	f1Hits = []
+	f2Hits = []
+	f3Hits = []
+	f4Hits = []
 
 	try:
 		if 'Sequences producing significant alignments:' in open(f1).read():
 			f1Hits = findHitSeqs(f1)
-			f1_hit = True
 		if 'Sequences producing significant alignments:' in open(f2).read():
 			f2Hits = findHitSeqs(f2)
-			f2_hit = True
 		if 'Sequences producing significant alignments:' in open(f3).read(): 
 			f3Hits = findHitSeqs(f3)
-			f3_hit = True
 		if 'Sequences producing significant alignments:' in open(f4).read(): 
 			f4Hits = findHitSeqs(f4)
-			f4_hit = True
 
 	except NotADirectoryError:
 		continue
 
-	if f1_hit & f3_hit:
-		chimReads1 = findReadPairs(f1Hits, f3Hits)
-		if(chimReads1):
-			print(" ")
-			print("%d forward chimeric reads found in cell %s" % (len(chimReads1), dir))
-			print(chimReads1)
-	if f2_hit & f4_hit:
-		chimReads2 = findReadPairs(f2Hits, f4Hits)
-		if(chimReads2):
-			print(" ")
-			print("%d reverse chimeric reads found in cell %s" % (len(chimReads2), dir))
-			print(chimReads2)
+	alk_hits = f1Hits + f2Hits
+	eml4_hits = f3Hits + f4Hits
+
+	chimReads = findReadPairs(alk_hits, eml4_hits)
+	if(chimReads):
+		print(" ")
+		print("Found %d chimeric reads found in cell %s" % (len(chimReads), dir))
+		print(chimReads)
 
 print(" ")
 
